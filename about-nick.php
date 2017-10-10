d1b8c907bc12fd55ba1b8733958d8d97088f592d
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-10 12:34:11 -0600

    first commit after Michael's mods

--- about-nick.php
+++ about-nick.php
@@ -2,13 +2,18 @@
 <html>
 	<head>
 		<title>A Page about me!</title>
-		<link href="https://bootcamp-coders.cnm.edu/~npascetti/css/about-nick.css" rel="stylesheet" type="text/css" />
+		<link href="https://bootcamp-coders.cnm.edu/~npascetti/css/about-nicksstylesheet.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<div class="main">
-			<h3 class="strike"> I'm Guybrush Threepwood, mighty pirate.</h3>
-			<h1>Hi! My name is Nick Pascetti!</h1>
-			<p>My name is Nick Pascetti, podcast addict, lover of beer, cats, esoteric LucasArts &trade; adventure games, in addition to being an aspiring web developer.</p>
+			<div class="heading">
+				<h3> <span class="strike">I'm Guybrush Threepwood, mighty pirate.</span></h3>
+				<h2>I'm Nick Pascetti, <span class="strike">and I'm selling these fine leather jackets.</span></h2>
+				<h1>I'm Nick Pascetti, aspiring web developer.</h1>
+			</div>
+			<p>Currently, I'm a podcast addict, maker of music, lover of craft (and non-craft) beers, cats, esoteric LucasArts &trade; adventure games, hiking and the outdoors ... <em>but</em> not necessarily in that order.</p>
+			<p>In the past I've worked as a gigging musician, in organic food and produce distribution, and conducting ecological restoration and surveying ... <em>but</em> not all simultaneously.</p>
+			<p>Looking to the future, I hope to start a career in web development ... <em>but</em> not until I've graduated from coding bootcamp! </p>
 			<div class="images">
 				<img src="https://bootcamp-coders.cnm.edu/~npascetti/images/nick.jpg" width="50%" height="50%" />
 			</div>
