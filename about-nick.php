36abdcfb5ea65dde47bca58ec3a282979dad24e5
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-09 21:03:51 -0600

    made strike class for strikethrough, maybe uploaded pic to daughter folder about-nick

---
+++ about-nick.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A Page about me!</title>
+		<link href="https://bootcamp-coders.cnm.edu/~npascetti/css/about-nick.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div class="main">
+			<h3 class="strike"> I'm Guybrush Threepwood, mighty pirate.</h3>
+			<h1>Hi! My name is Nick Pascetti!</h1>
+			<p>My name is Nick Pascetti, podcast addict, lover of beer, cats, esoteric LucasArts &trade; adventure games, in addition to being an aspiring web developer.</p>
+			<div class="images">
+				<img src="https://bootcamp-coders.cnm.edu/~npascetti/images/nick.jpg" width="50%" height="50%" />
+			</div>
+		</div>
+	</body>
+</html>
